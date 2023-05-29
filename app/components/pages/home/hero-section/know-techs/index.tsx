e1bcbed0917c483ca22822f1ead34a7a82d3e442
JoseFilipeVieiraFurieri <jf.furieri@ymail.com>
2023-05-29 17:35:04 -0300

    feat:adicionando seção de conhecimentos

---
+++ app/components/pages/home/hero-section/know-techs/index.tsx
@@ -0,0 +1,26 @@
+import { SectionTitle } from '@/app/components/section-title'
+import { TbBrandNextjs } from 'react-icons/tb'
+import { KnownTech } from './known-tech'
+
+export const KnowTechs = () => {
+  return (
+    <section className="container py-16">
+      <SectionTitle subtitle="competências" title="conhecimentos" />
+      <div className="grid grid-cols-[repeat(auto-fit, minmax(264px, 1fr))] gap-3">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <KnownTech
+            tech={{
+              icon: <TbBrandNextjs />,
+              name: 'Next.js',
+              startDate: '2023-04-01',
+            }}
+            key={`name/${index}`}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+// grid-cols-[repeat(auto-fit, minmax(264px, 1fr))] esse é um jeitp de quebrar o card de conhecimento. isso vai fazer
+// o cartão de repetir em coluna até o valor mimnimo estipulado(264px) abaixo disse ele quebra a fila
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from './components/pages/home/hero-section'
 import { Header } from './components/header'
+import { KnowTechs } from './components/pages/home/hero-section/know-techs'
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
+      <KnowTechs />
     </>
   )
 }
